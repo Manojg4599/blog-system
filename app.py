f0d38a26054e0a5f227d3f822b86d33b804db308
Manojg4599 <48507575+Manojg4599@users.noreply.github.com>
2026-03-04 14:22:30 +0530

    Fix import statement for Config in app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from config import Config from flask import Flask, render_template
 import os
 from datetime import datetime, timedelta
 from config import Config
